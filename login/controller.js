66aa83830cb18b62b5c6619f85bb2589f2b54941
tamara_danilova <roznica14@yandex.ru>
2024-08-01 17:46:30 +0400

    fix auth

--- login/controller.js
+++ login/controller.js
@@ -9,28 +9,30 @@ module.exports.loginAdmin = async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        // Проверка наличия данных в запросе
+        console.log(username);
+        console.log(password);
+
+        console.log('Admin Username:', adminUsername);
+        console.log('Admin Password Hash:', adminPasswordHash);
+
         if (!username || !password) {
             return res.status(400).json({ message: 'Username and password are required' });
         }
 
-        // Проверка имени пользователя
         if (username !== adminUsername) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        // Асинхронная проверка пароля
-        const isMatch = await bcrypt.compare(password, adminPasswordHash);
+        const isMatch = await bcrypt.compare(password, bcrypt.hashSync(adminPasswordHash, 10));
+
         if (!isMatch) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        // Проверка наличия секретного ключа
         if (!secretKey) {
             return res.status(500).json({ message: 'Server configuration error: JWT secret not set' });
         }
 
-        // Генерация JWT
         const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
         res.json({ token });
     } catch (error) {
